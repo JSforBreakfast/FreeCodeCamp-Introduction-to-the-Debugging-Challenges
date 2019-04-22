93bf1d360eb0cd94a67c99308f7c99fb7d101fb8
Vitor Emanuel <veslima3@gmail.com>
2019-04-22 12:18:44 -0300

    Catch Missing Open and Closing Parenthesis After a Function Call
    When a function or method doesn't take any arguments, you may forget to include the (empty) opening and closing parentheses when calling it. Often times the result of a function call is saved in a variable for other use in your code. This error can be detected by logging variable values (or their types) to the console and seeing that one is set to a function reference, instead of the expected value the function returns.
    
    The variables in the following example are different:
    
    function myFunction() {
      return "You rock!";
    }
    let varOne = myFunction; // set to equal a function
    let varTwo = myFunction(); // set to equal the string "You rock!"
    
    Fix the code so the variable result is set to the value returned from calling the function getNine.

---
+++ Catch Missing Open and Closing Parenthesis After a Function Call.js
@@ -0,0 +1,19 @@
+let x = 6;
+let y = 3;
+let result = getNine();
+function getNine() {
+  return x + y;
+}
+
+
+console.log(result);
+
+/*let x = 6;
+let y = 3;
+let result = x+y;
+function getNine() {
+  return getNine();
+}
+
+
+console.log(result); */
